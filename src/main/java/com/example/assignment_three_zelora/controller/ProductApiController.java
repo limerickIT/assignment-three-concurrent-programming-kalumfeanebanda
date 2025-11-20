c8d9a82a31d436c70b94e9669e069cd2c70dd617
kalumfeanebanda <K00300841@student.tus.ie>
2025-11-20 00:24:51 +0000

    filter set up in react

--- src/main/java/com/example/assignment_three_zelora/controller/ProductApiController.java
+++ src/main/java/com/example/assignment_three_zelora/controller/ProductApiController.java
@@ -4,6 +4,8 @@ import com.example.assignment_three_zelora.model.entitys.Product;
 import com.example.assignment_three_zelora.model.service.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.math.BigDecimal;
+
 
 import com.example.assignment_three_zelora.model.dto.ProductSummaryDto;
 
@@ -20,12 +22,31 @@ public class ProductApiController {
         this.productService = productService;
     }
 
-    // GET http://localhost:8080/api/products/search
     @GetMapping("/search")
-    public List<ProductSummaryDto> searchProducts() {
-        // for now: just return all summaries
-        return productService.getAllProductSummaries();
+    public List<ProductSummaryDto> searchProducts(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String keyword,
+            @RequestParam(required = false) Integer categoryId,
+            @RequestParam(required = false) Integer supplierId,
+            @RequestParam(required = false) BigDecimal minPrice,
+            @RequestParam(required = false) BigDecimal maxPrice,
+            @RequestParam(required = false) BigDecimal minDiscount,
+            @RequestParam(required = false) BigDecimal maxDiscount,
+            @RequestParam(required = false) Integer minRating,
+            @RequestParam(required = false) String manufacturer,
+            @RequestParam(required = false) String material,
+            @RequestParam(required = false) String colour,
+            @RequestParam(required = false) String size,
+            @RequestParam(required = false) Boolean recent
+    ) {
+        return productService.searchProducts(
+                name, keyword, categoryId, supplierId,
+                minPrice, maxPrice, minDiscount, maxDiscount,
+                minRating, manufacturer, material, colour, size,
+                recent
+        );
     }
+
 
     // GET http://localhost:8080/api/products/1
     @GetMapping("/{id:\\d+}")
